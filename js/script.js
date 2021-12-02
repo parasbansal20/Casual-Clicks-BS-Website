ffe751c2c102fa7040346e213606fae9deaddcec
parasbansal20 <parasbansal20@gmail.com>
2021-12-02 20:42:52 +0530

    added google analytics

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const options = {
+  top: '32px', // default: '32px'
+  right: 'unset', // default: '32px'
+  left: '32px', // default: 'unset'
+  time: '0.5s', // default: '0.3s'
+  mixColor: '#fff', // default: '#fff'
+  backgroundColor: '#fff',  // default: '#fff'
+  buttonColorDark: '#100f2c',  // default: '#100f2c'
+  buttonColorLight: '#fff', // default: '#fff'
+  saveInCookies: false, // default: true,
+  label: 'X', // default: ''
+  autoMatchOsTheme: true // default: true
+}
+
+const darkmode = new Darkmode(options);
+darkmode.showWidget();
